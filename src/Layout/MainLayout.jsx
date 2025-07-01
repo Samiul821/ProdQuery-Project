f10b35bbcf5f12789998cf8bac2eafa364a9c3c1
Samiul821 <smsamiul821@gmail.com>
2025-07-01 10:55:31 +0600

    check

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <header>
-                <Navbar />
-            </header>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header>
+        <Navbar />
+      </header>
 
-            <main className="flex-grow min-h-[calc(100vh-200px)]">
-                <Outlet />
-            </main>
+      <main className="flex-grow min-h-[calc(100vh-200px)]">
+        <Outlet />
+      </main>
 
-            <footer>
-                <Footer />
-            </footer>
-        </div>
-    );
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default MainLayout;
